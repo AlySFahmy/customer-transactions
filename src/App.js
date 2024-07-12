a5451e697da9aa357035d34b9fc580068a76e7b6
AlySFahmy <alyfahmy507@gmail.com>
2024-07-12 19:41:07 +0300

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-// import React, { useState } from 'react';
-// import CustomerTable from './components/CustomerTable';
-// import TransactionChart from './components/TransactionChart';
-// import './App.css';
-
-// function App() {
-//   const [selectedCustomer, setSelectedCustomer] = useState(null);
-//   const [customerTransactions, setCustomerTransactions] = useState([]);
-
-//   const handleSelectCustomer = (customer, transactions) => {
-//     setSelectedCustomer(customer);
-//     setCustomerTransactions(transactions);
-//   };
-
-//   return (
-//     <div className="container my-5">
-//       <h1 className="text-center mb-4">Customer Transactions</h1>
-//       <div className="row">
-//         <div className="col-md-6 mb-4">
-//           <CustomerTable onSelectCustomer={handleSelectCustomer} />
-//         </div>
-//         {selectedCustomer && (
-//           <div className="col-md-6">
-//             <TransactionChart customer={selectedCustomer} transactions={customerTransactions} />
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 
 
 import React, { useState } from 'react';
